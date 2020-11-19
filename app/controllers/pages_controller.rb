b4e07c735f91169c8bce1a09b2f8e47a327f3fa9
Tim Whitacre <tim@timw.co>
2020-11-19 07:09:03 -0500

    Updated index page with projects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
 
   def index
     @description = "Yo 👋! Welcome to the home of Tim Whitacre, an engineer turned entrepreneur based in Atlanta. Let's make things."
-    @articles = Article.order("published_at ASC").last(3).reverse
+    @latest_articles = Article.order("published_at ASC").last(3).reverse
+    @active_projects = Project.order('started ASC').where(active: true)
   end
 
   def about
