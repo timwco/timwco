020d70f6458ca0329cd7f260a6f1b07088816dd1
Tim Whitacre <tim@timw.co>
2022-08-24 11:13:58 -0400

    Bumped to Rails 6.x

--- Gemfile
+++ Gemfile
@@ -1,9 +1,9 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '3.0.2'
+ruby "3.1.2"
 
-gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
+gem "rails", "~> 7.0.2", ">= 7.0.2.3"
 gem 'pg'
 gem 'puma', '~> 4.1'
 gem 'sass-rails', '>= 6'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_135115) do
+ActiveRecord::Schema.define(version: 2022_08_24_120924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
