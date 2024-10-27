d0c52c70ca458fed0140fe783e4e61e387b11930
Tim Whitacre <tim@timw.co>
2024-10-27 12:08:32 -0500

    Added wishlist category

--- app/models/wishlist_item.rb
+++ app/models/wishlist_item.rb
@@ -1,4 +1,4 @@
 class WishlistItem < ApplicationRecord
-  enum category: [:games, :kitchen, :tech, :books, :misc, :sports, :lego]
+  enum category: [:games, :kitchen, :tech, :books, :misc, :sports, :lego, :music]
   enum price: ["$", "$$", "$$$"]
 end
